8c836331e4d10d60f34c23ed1af56961413547a7
X-Rian02 <69094660+X-Rian02@users.noreply.github.com>
2020-08-05 22:24:47 +0700

    Add files via upload

---
+++ Mata02.sh
@@ -0,0 +1,122 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo "Memulai Tools Mata-Mata" | lolcat
+echo "[*]Loading" | lolcat
+echo ">>>" | lolcat
+echo ">>>>>" | lolcat
+echo ">>>>>>>>>" | lolcat
+echo ">>>>>>>>>>>>>>" | lolcat
+echo ">>>>>>>>>>>>>>>>>>>" | lolcat
+echo "[*]Done"
+toilet -f big -F gay Welcome
+clear
+echo $red    "•───────────────────────────────────────────────•"
+echo $yellow⠀⠀⠀ "•─────────────•By X_Ryn7Gans•───────────────•"
+echo $red    "•───────────────────────────────────────────────•"
+echo $cyan⠀⠀⠀ "•────────────•Tools Mata-Mata•──────────────•"
+echo $red    "•───────────────────────────────────────────────•"
+echo
+echo
+date | lolcat
+echo
+echo $green "[*]"$cyan"Author     : "$yellow"DPC~X_Ryn7"
+echo $cyan  "[*]"$red"Team       : "$green"Decepticon Pirate Cyber"
+echo $purple "[*]"$yellow"Contact Me : "$cyan"+6285700727407"
+echo
+echo $purple "•─────•Silahkan Pilih•─────•"
+echo
+echo $blue "[*]1 Sql Injection"
+echo $red "•─────••───────────────••─────•"
+echo $yellow "[*]2 Pentester"
+echo $red "•─────••───────────────••─────•"
+echo $purple "[*]3 Admin Finder"
+echo $red "•─────••───────────────••─────•"
+echo $cyan "[*]4 Distributed Denial of Service"
+echo $red "•─────••───────────────••─────•"
+echo $green "[*]5 Hack CCTV"
+echo $red "•─────••───────────────••─────•"
+echo $yellow "[*]6 Metasploit Framework"
+echo $red "•─────••───────────────••─────•"
+echo $yellow "[*]7 Exit"
+echo $red "•─────••───────────────••─────•"
+sleep 2
+read -p"[*]Enter Select : " bro
+echo $green
+#batas anjing
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+echo $yellow"[*]Tekan CTRL+C Untuk Membatalkan"
+pkg install python2 -y
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+sleep 3
+# Batas Anjing
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+echo $yellow"[*]]Tekan CTRL+C Untuk Membatalkan"
+pkg install php -y
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+ls
+php rhawk.php
+fi
+sleep 4
+# Batas Anjing
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+echo $yellow"[*]Tekan CTRL+C Untuk Membatalkan"
+pkg install php -y
+git clone https://github.com/mrcakil/admin_finder 
+cd admin_finder
+php adfin.php
+fi
+sleep 5
+# Batas Anjing
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+echo $yellow"[*]Tekan CTRL+C Untuk Membatalkan"
+pkg install python -y
+git clone http://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+sleep 6
+# Batas Anjing
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+echo $yellow"[*]Tekan CTRL+C Untuk Membatalkan"
+pkg install python2 -y
+pip2 install requests
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+sleep 7
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+echo $yellow"[*]Tekan CTRL+C Untuk Membatalkan"
+echo $green"[*]Proses Install Selama 20-30 Detik Mohon Sabar :)"
+pkg install curl -y
+curl -LO https://raw.githubusercontent.com/Techzindia/Metasploit_For_Termux/master/metasploitTechzindia.sh
+sh metasploitTechzindia.sh
+cd metasploit-framework
+./msfconsole
+fi
+sleep 8
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+toilet -f big -F gay Thanks
+echo
+exit
+fi
+             
